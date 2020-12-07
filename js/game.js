2b6fe15bbf5578a3d0a3c7928313be910d57f3b0
Mathura <mathuradas333@gmail.com>
2020-12-07 07:40:14 +0530

    [Mathura]Add. Ability to show label of first and current chance Info

--- js/game.js
+++ js/game.js
@@ -1,4 +1,14 @@
-let choice = 'X';
+let playerName = 'Mathura'
+let choice = 'O';
+let chances = ['User','Computer'];
+let firstChance = chances[0];
+let currentChance = chances[0];
+window.addEventListener('DOMContentLoaded', (event) => {
+  loadPlayerInfo();
+  loadFirstChanceInfo();
+  loadCurrentChanceInfo();
+  setTimeout(()=>document.getElementById("firstChance").style.opacity = "0",5000);
+});
 function animateChar(node) {
   if(choice==='X'){
     $(`.${node.id}.cross`).toggleClass('cross-shown')
@@ -12,4 +22,30 @@ function onAboutClick(){
 }
 function getRules(){
   window.location = site_properties.rules_page;
+}
+function newUserSignup(){
+  window.location = site_properties.pickChoice_page;
+}
+function loadPlayerInfo(){
+  let compChoice;
+  if(choice==='X')
+  compChoice = 'O';
+  else if(choice==='O')
+  compChoice = 'X';
+  else
+  compChoice = ' ';
+
+  let innerHtml = `
+  <div class="player-name"> PLAYER NAME: ${playerName}</div>
+  <div class="player-sign">PLAYER'S CHOICE: ${choice}</div>
+  <div class="computer-sign">COMPUTER'S CHOICE: ${compChoice}</div>
+`;
+  document.querySelector('#player-info').innerHTML = innerHtml
+}
+function loadFirstChanceInfo(){
+  document.querySelector('.firstChance').innerHTML = `${firstChance} got first chance`;
+}
+function loadCurrentChanceInfo(){
+  document.getElementById("currentChance").style.visibility = "visible";
+  document.querySelector('.currentChance').innerHTML = `It's ${currentChance}'s turn`;
 }
