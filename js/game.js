183ba3ea7a15228f52caeb0b8edef80f9e36d92d
Mathura <mathuradas333@gmail.com>
2020-12-07 14:33:19 +0530

    [Mathura]Add. Ability to take user name in input and to use it in game page

--- js/game.js
+++ js/game.js
@@ -1,39 +1,44 @@
-let playerName = 'Mathura'
-let choice =  localStorage.getItem('userChoice');
-let chances = ['User','Computer'];
+let playerName = localStorage.getItem('userName');
+choice = localStorage.getItem('userChoice');
+let chances = ['User', 'Computer'];
 let firstChance = chances[0];
 let currentChance = chances[0];
 window.addEventListener('DOMContentLoaded', (event) => {
   loadPlayerInfo();
   loadFirstChanceInfo();
   loadCurrentChanceInfo();
-  setTimeout(()=>document.getElementById("firstChance").style.opacity = "0",5000);
+  setTimeout(() => document.getElementById("firstChance").style.opacity = "0", 5000);
 });
+
 function animateChar(node) {
-  if(choice==='X'){
+  if (choice === 'X') {
     $(`.${node.id}.cross`).toggleClass('cross-shown')
   }
-  if(choice==='O'){
+  if (choice === 'O') {
     $(`.${node.id}.circle`).toggleClass('circle-shown')
   }
 }
-function onAboutClick(){
+
+function onAboutClick() {
   window.location = site_properties.about_page;
 }
-function getRules(){
+
+function getRules() {
   window.location = site_properties.rules_page;
 }
-function newUserSignup(){
+
+function newUserSignup() {
   window.location = site_properties.pickChoice_page;
 }
-function loadPlayerInfo(){
+
+function loadPlayerInfo() {
   let compChoice;
-  if(choice==='X')
-  compChoice = 'O';
-  else if(choice==='O')
-  compChoice = 'X';
+  if (choice === 'X')
+    compChoice = 'O';
+  else if (choice === 'O')
+    compChoice = 'X';
   else
-  compChoice = ' ';
+    compChoice = ' ';
 
   let innerHtml = `
   <div class="player-name"> PLAYER NAME: ${playerName}</div>
@@ -42,10 +47,12 @@ function loadPlayerInfo(){
 `;
   document.querySelector('#player-info').innerHTML = innerHtml
 }
-function loadFirstChanceInfo(){
+
+function loadFirstChanceInfo() {
   document.querySelector('.firstChance').innerHTML = `${firstChance} got first chance`;
 }
-function loadCurrentChanceInfo(){
+
+function loadCurrentChanceInfo() {
   document.getElementById("currentChance").style.visibility = "visible";
   document.querySelector('.currentChance').innerHTML = `It's ${currentChance}'s turn`;
 }
--- js/pickChoice.js
+++ js/pickChoice.js
@@ -1,4 +1,5 @@
 let choiceBool = false;
+let userName = 'User';
 var choice;
 window.addEventListener('DOMContentLoaded', (event) => {
 
@@ -8,23 +9,38 @@ function selectChoice(button) {
     if (button.id == 'cross') {
         choiceBool = true;
         document.querySelector("#choiceChar").textContent = 'X';
-        document.querySelector(".error-output").textContent = ''; 
+        document.querySelector(".error-output").textContent = '';
     }
     if (button.id == 'circle') {
         choiceBool = true;
         document.querySelector("#choiceChar").textContent = 'O';
-        document.querySelector(".error-output").textContent = ''; 
+        document.querySelector(".error-output").textContent = '';
     }
 }
 
 function continueOnClick() {
-    if (choiceBool === true) {
-        window.location.replace(site_properties.game_page);
+    if (choiceBool === true && document.getElementById("name").value) {
         choice = document.querySelector("#choiceChar").textContent;
+        userName = document.getElementById("name").value;
         localStorage.removeItem('userChoice')
+        localStorage.removeItem('userName')
         localStorage.setItem('userChoice', choice);
-    }
-    else{
-        document.querySelector(".error-output").textContent = 'You need to Choose any one Option'; 
+        localStorage.setItem('userName', userName);
+        window.location.replace(site_properties.game_page);
+    } else if (!choiceBool) {
+        document.querySelector(".error-output").textContent = 'You need to Choose any one Option';
+    } else {
+        document.getElementById('error-output').style.marginLeft = "28.7%";
+        document.querySelector(".error-output").textContent = 'Name is required';
     }
 }
+
+$('input').on('focusin', function () {
+    $(this).parent().find('label').addClass('active');
+});
+
+$('input').on('focusout', function () {
+    if (!this.value) {
+        $(this).parent().find('label').removeClass('active');
+    }
+});
