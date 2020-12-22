1e8e50a00ec258b1717f1917da56cbb3c6b133a3
CMD <gautamkailashsingh@gmail.com>
2020-12-22 11:03:55 +0530

    [Gautam] Refactored location of computer algo

--- tic-tac-toe-app/src/js/game.js
+++ tic-tac-toe-app/src/js/game.js
@@ -8,11 +8,8 @@ window.addEventListener("DOMContentLoaded", (event) => {
 const uri = 'http://localhost:5000/game/add';
 let playerName = localStorage.getItem("userName");
 let choice = localStorage.getItem("userChoice");
-let computerChoice=choice==="X"?"O":"X";//new
-//let board= ['_', '_', '_', '_', '_', '_', '_', '_', '_'];//new
-//let board= [0,1,2,3,4,5,6,7,8];
+let computerChoice=choice==="X"?"O":"X";
 let board = [0,0,0,0,0,0,0,0,0];
-//var g=board;//new
 let turn='uesr';
 let chances = ["User", "Computer"];
 let random = Math.floor(Math.random() * chances.length);
@@ -33,18 +30,6 @@ let userArray = [];
 let computerArray = [];
 let check=0;
 
-/*if(firstChance==="Computer" && check===0)
-{
-	check=1;
-	let selectedCellIndex = Math.floor(Math.random() * 9);
-	computerArray.push((selectedCellIndex)+1);
-  	board[selectedCellIndex]=computerChoice;
-  	let n='block_'+selectedCellIndex;
-  	animateChar(n, computerChoice);
-  	turn='User';
-  	//getGameSituation();
-}*/
-
 export function UpdateNameAndChoice() {
   playerName = localStorage.getItem("userName");
   choice = localStorage.getItem("userChoice");
@@ -53,7 +38,6 @@ export function UpdateNameAndChoice() {
   firstChance = chances[random];
   restart();
   loadPlayerInfo();
-  // setTimeout(() => (document.getElementById("firstChance").style.opacity = "0"),8000);
   loadFirstChanceInfo();
 }
 export function AboutCreators() {
@@ -107,10 +91,8 @@ function animateChar(node, print) {
 function addCellIntoUserArray(node) {
   let selectedCellIndex = parseInt(node.charAt(node.length - 1));
   userArray.push(selectedCellIndex + 1);
-  //board[selectedCellIndex]=choice;//new
-  board[selectedCellIndex]=1;//new
+  board[selectedCellIndex]=1;
   turn='Computer';
-  //getGameSituation();
 }
 
 function getGameSituation() {
@@ -131,7 +113,6 @@ function getGameSituation() {
     displayGameSituationInLabel("TIED");
   }
   else if(turn==='Computer'){
-	//addCellIntoComputerArray();
 	response();
   }
 }
@@ -158,7 +139,6 @@ export function restart() {
   $(".circle-shown").removeClass("circle-shown");
   userArray = [];
   computerArray = [];
-  //board= ['_', '_', '_', '_', '_', '_', '_', '_', '_'];
   board = [0,0,0,0,0,0,0,0,0];
   check=0;
   document.getElementById("gameSituation").textContent = "";
@@ -188,19 +168,6 @@ export function loadFirstChanceInfo() {
 	{
 		response();
 	}
-	/*if(firstChance==="Computer" && check===0)
-	{
-		restart();
-		let selectedCellIndex = Math.floor(Math.random() * 9);
-		computerArray.push((selectedCellIndex)+1);
-		board[selectedCellIndex]=computerChoice;
-		let n='block_'+selectedCellIndex;
-		animateChar(n, computerChoice);
-		turn='User';
-		firstChance="User";
-		//restart();
-		check=1;
-	}*/
   	
 }
 
@@ -209,442 +176,4 @@ export function loadCurrentChanceInfo() {
   document.querySelector(
     "#currentChance"
   ).innerHTML = `It's ${currentChance}'s turn`;
-}
-
-
-
-
-
-/*
-
-//Computer's Move
-function addCellIntoComputerArray() {
-  let selectedCellIndex = findBestMove();
-  //let selectedCellIndex=minimax(board, computerChoice);
-  //computerArray.push(changeIndex(selectedCellIndex)+1);
-  computerArray.push((selectedCellIndex)+1);
-  board[selectedCellIndex]=computerChoice;
-  let n='block_'+selectedCellIndex;
-  animateChar(n, computerChoice);
-  turn='User';
-  getGameSituation();
-}
-
-/*function changeIndex(selectedCellIndex)
-{
-	switch(selectedCellIndex)
-	{
-		case 0:
-			return 0;
-		case 1:
-			return 3;
-		case 2:
-			return 6;
-		case 3:
-			return 1;
-		case 4:
-			return 4;
-		case 5:
-			return 7;
-		case 6:
-			return 2;
-		case 7:
-			return 5;
-		case 8:
-			return 8;
-		default:
-			return 0;																																														
-	}
-}
-
-let player = computerChoice, opponent = choice; 
-let computerMove=0;
-
-// This function returns true if there are moves 
-// remaining on the board. It returns false if 
-// there are no moves left to play. 
-function isMovesLeft(board) 
-{ 
-	for(let i=0;i<9;i++)
-		if(board[i] === "_")
-		return true; 
-	return false;
-} 
-
-// This is the evaluation function
-function evaluate(b) 
-{ 
-	// Checking for Rows for X or O victory. 
-	for (let row = 0; row < 3; row++) 
-	{ 
-		if (b[0+3*row] === b[1+3*row] && b[1+3*row] === b[2+3*row]) 
-		{ 
-			if (b[0+3*row] === player) 
-				return +10; 
-			else if (b[0+3*row] === opponent) 
-				return -10; 
-		} 
-	} 
-
-	// Checking for Columns for X or O victory. 
-	for (let col = 0; col < 3; col++) 
-	{ 
-		if (b[3*0+col] === b[3*1+col] && b[3*1+col] === b[3*2+col]) 
-		{ 
-			if (b[3*0+col] === player) 
-				return +10; 
-
-			else if (b[3*0+col] === opponent) 
-				return -10; 
-		} 
-	} 
-
-	// Checking for Diagonals for X or O victory. 
-	if (b[0] === b[4] && b[4] === b[8]) 
-	{ 
-		if (b[0] === player) 
-			return +10; 
-		else if (b[0] === opponent) 
-			return -10; 
-	} 
-
-	if (b[2] === b[4] && b[4] === b[6]) 
-	{ 
-		if (b[2] === player) 
-			return +10; 
-		else if (b[2] === opponent) 
-			return -10; 
-	} 
-
-	// Else if none of them have won then return 0 
-	return 0; 
-} 
-
-// This is the minimax function. It considers all 
-// the possible ways the game can go and returns 
-// the value of the board 
-function minimax(board,depth,isMax) 
-{ 
-	let score = evaluate(board); 
-
-	// If Maximizer has won the game 
-	// return his/her evaluated score 
-	if (score === 10) 
-		return score; 
-
-	// If Minimizer has won the game 
-	// return his/her evaluated score 
-	if (score === -10) 
-		return score; 
-
-	// If there are no more moves and 
-	// no winner then it is a tie 
-	if (isMovesLeft(board) === false) 
-		return 0; 
-
-	// If this maximizer's move 
-	if (isMax) 
-	{ 
-		let best = -1000; 
-
-		// Traverse all cells 
-		for (let i = 0; i < 3; i++) 
-		{ 
-			for (let j = 0; j < 3; j++) 
-			{ 
-				// Check if cell is empty 
-				if (board[i*3+j]==='_') 
-				{ 
-					// Make the move 
-					board[i*3+j] = player; 
-
-					// Call minimax recursively and choose 
-					// the maximum value 
-					best = Math.max(best, minimax(board, 
-									depth + 1, !isMax)); 
-
-					// Undo the move 
-					board[i*3+j] = '_'; 
-				} 
-			} 
-		} 
-		return best+depth; 
-	} 
-
-	// If this minimizer's move 
-	else
-	{ 
-		let best = 1000; 
-
-		// Traverse all cells 
-		for (let i = 0; i < 3; i++) 
-		{ 
-			for (let j = 0; j < 3; j++) 
-			{ 
-				// Check if cell is empty 
-				if (board[i*3+j] === '_') 
-				{ 
-					// Make the move 
-					board[i*3+j] = opponent; 
-
-					// Call minimax recursively and choose 
-					// the minimum value 
-					best = Math.min(best, minimax(board, 
-									depth + 1, !isMax)); 
-
-					// Undo the move 
-					board[i*3+j] = '_'; 
-				} 
-			} 
-		} 
-		return best; 
-	} 
-} 
-
-// This will return the best possible 
-// move for the player 
-function findBestMove() 
-{ 
-	let bestVal = -1000; 
-
-	// Traverse all cells, evaluate minimax function 
-	// for all empty cells. And return the cell 
-	// with optimal value. 
-	for (let i = 0; i < 3; i++) 
-	{ 
-		for (let j = 0; j < 3; j++) 
-		{ 
-			// Check if cell is empty 
-			if (board[i*3+j] === '_') 
-			{ 
-				// Make the move 
-				board[i*3+j] = player; 
-
-				// compute evaluation function for this 
-				// move. 
-				let moveVal = minimax(board, 0, false); 
-
-				// Undo the move 
-				board[i*3+j] = '_'; 
-
-				// If the value of the current move is 
-				// more than the best value, then update 
-				// best/ 
-				if (moveVal > bestVal) 
-				{ 
-          			computerMove=3*i+j;
-          			bestVal = moveVal; 
-				} 
-			} 
-		} 
-	}
-
-	return computerMove; 
-} */
-
-
-
-
-//New Algorithm
-
-
-//winning positions
-var pos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]];
-//score
-var countLost = 0;
-var countTies = 0;
-
-//computer intelligence figures best response
-function response(){
-    var p;
-
-    function claim(o){
-        board[o] = 2;
-		//document.getElementById("t"+o).className+="o";
-		let n='block_'+(o);
-		  animateChar(n, computerChoice);
-		  turn = "User";
-		  computerArray.push(o+1);
-		  getGameSituation();
-        return;
-    }
-
-    for (var w=0; w<pos.length;w++){
-        p = pos[w];
-        var countX =0;
-        var countO =0;
-        var blankO = -1;
-        for (var c = 0; c < 3; c++){
-            if (board[p[c]] == 1) countX++;
-            if (board[p[c]] == 2) countO++;
-            if (board[p[c]] == 0) blankO = c;
-        }
-
-        //1 ensure win when pc has 2 in a row and blank
-        if (countO==2 && blankO > -1){
-            claim(p[blankO]);
-            return;
-        }
-
-    }
-
-    for (var w=0; w<pos.length;w++){
-        p = pos[w];
-        var countX =0;
-        var countO =0;
-        var blankO = -1;
-        for (var c = 0; c < 3; c++){
-            if (board[p[c]] == 1) countX++;
-            if (board[p[c]] == 2) countO++;
-            if (board[p[c]] == 0) blankO = c;
-        }
-
-        //2 avert sure loss when user has 2 in a row and blank
-         if (countX==2 && blankO > -1){
-             claim(p[blankO]);
-            return;
-        }
-
-    }
-
-    //3 claim optimal middle if open
-    if (board[4] == 0){
-        claim(4);
-        return;
-    }
-	
-	//avert diagonal xox => claim edge, not corner
-	if(board.toString()==[0, 0, 1, 0, 2, 0, 1, 0, 0].toString()){
-	    claim(7);
-        return;
-    }
-	if(board.toString()==[1, 0, 0, 0, 2, 0, 0, 0, 1].toString()){
-        claim(5);
-        return;
-    }
-	//avoid corner trap
-	if(board.toString()==[1, 0, 0, 0, 2, 0, 0, 1, 0].toString()){
-	    claim(6);
-        return;
-    }
-	
-	
-	 for (var w=0; w<pos.length;w++){
-        p = pos[w];
-        var countX =0;
-        var countO =0;
-        var blankO = -1;
-        for (var c = 0; c < 3; c++){
-            if (board[p[c]] == 1) countX++;
-            if (board[p[c]] == 2) countO++;
-            if (board[p[c]] == 0) blankO = c;
-        }
-
-        //4 avert user 2 in row when 1 in row and 2 blank by placing O in a corner
-		if (countX==1 && countO==0 && blankO > -1){
-            
-			
-			if (p[blankO]==0 && board[0] == 0){
-				claim(0);
-				return;
-			}
-			if (p[blankO]==2 && board[2] == 0){
-				claim(2);
-				return;
-			}
-			if (p[blankO]==6 && board[6] == 0){
-				claim(6);
-				return;
-			}
-			if (p[blankO]==8 && board[8] == 0){
-				claim(8);
-				return;
-			}
-			
-        }
-    }
-	
-	
-	
-
-	
-    for (var w=0; w<pos.length;w++){
-        p = pos[w];
-        var countX =0;
-        var countO =0;
-        var blankO = -1;
-        for (var c = 0; c < 3; c++){
-            if (board[p[c]] == 1) countX++;
-            if (board[p[c]] == 2) countO++;
-            if (board[p[c]] == 0) blankO = c;
-        }
-
-        //5 support yourself to 2 in a row when 1 in row and 2 blank
-         if (countO==1 && countX==0 && blankO > -1){
-		 
-             claim(p[blankO]);
-            return;
-        }
-
-    }
-
-
-    for (var w=0; w<pos.length;w++){
-        p = pos[w];
-        var countX =0;
-        var countO =0;
-        var blankO = -1;
-        for (var c = 0; c < 3; c++){
-            if (board[p[c]] == 1) countX++;
-            if (board[p[c]] == 2) countO++;
-            if (board[p[c]] == 0) blankO = c;
-        }
-
-        //6 avert user 2 in row when 1 in row and blank
-     if (countX==1 && blankO > -1){
-            claim(p[blankO]);
-            return;
-        }
-    }
-
-    //all taken
-    countTies++;
-    resetBoard()
-
-}
-
-function checkWin(){
-    var p;
-    for (var w=0; w<pos.length;w++){
-        p = pos[w];
-
-        if ( board[p[0]] == board[p[1]] && board[p[0]] == board[p[2]] && board[p[0]] > 0){
-
-            for (var i=0;i<3;i++)
-                document.getElementById("t"+p[i]).className+=" win";
-
-            if (board[p[0]] == 1) {
-                alert("You actually won!")
-            }
-            if (board[p[0]] == 2) {
-                countLost++;
-            }
-
-            resetBoard();
-            return 1;
-        }
-    }
-}
-
-function resetBoard(){
-	board = [0,0,0,0,0,0,0,0,0];
-	//document.getElementById('score-o').innerText = countLost;
-	//document.getElementById('score-g').innerText = countTies;
-	
-    //setTimeout(function(){
-    //    for (var i = 0; i < 9; i++)
-    //        document.getElementById("t" + i).className = "";
-	//	
-	//	g = [0,0,0,0,0,0,0,0,0];
-    //}, 500);
 }
